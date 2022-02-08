27b53881861c0685155dc1538bfe03c6d445c6f3
kcerbulis <kcerbulis@gmail.com>
2022-02-08 17:38:53 +0000

    HelloWorld lab task, week2

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TextInput, Button, FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class todo extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      items: [],
+      temp_item: ""
+    }
+  }
+
+  addItemToList = () => {
+    let newItems = this.state.items.concat(this.state.temp_item);
+    this.setState({
+      items: newItems,
+      temp_item: ""
+    });
+  }
+
+  remove = (index) => {
+    console.log(index);
+    let newList = this.state.items;
+    newList.splice(index, 1);
+    this.setState({items: newList});
+  }
+
+  render(){
+    return (
+      <View>
+        <TextInput
+          placeholder='Add to list'
+          onChangeText={value => this.setState({temp_item: value})}
+          value={this.state.temp_item}
+        />
+        <Button
+          onPress={() => {
+            this.addItemToList();
+          }}
+          title="Add"
+        />
+
+        <FlatList
+          data={this.state.items}
+          renderItem={({item, index}) =>
+            <View>
+              <Text>{item}</Text>
+              <Button
+                onPress={() => this.remove(index)}
+                title="Done"
+              />
+            </View>
+          }
+        />
+      </View>
+    )
+  }
+
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default todo;
